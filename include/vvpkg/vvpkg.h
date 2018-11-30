589805e594573a7a687094f24690842930cce517
DirectorOmega <directoromega@protonmail.com>
2018-11-30 01:23:01 -0600

    Building test cases to define and test the lip integrations, I need to setup a nice clean way to hide the unordered nature of the chunks in the bin behind the traditional file stream. I have some notes in the source of tests/test_LIPIntegration.cc on how I am approaching that, I have it working with the single chunk example but when the chunks are unordered the buffer operation wont work for the stream so read operations that cross chunk boundries when the chunks are stored out of order will break. I need to change how the buffer for the stream is filled, I may be able to keep it FILE level and make it work by overriding the underflow method but I think i should look into the other options before I go that route.

--- include/vvpkg/vvpkg.h
+++ include/vvpkg/vvpkg.h
@@ -1,2 +1,36 @@
 #include "repository.h"
 #include "rabin_boundary.h"
+#include <stdex/defer.h>
+
+#include <vvpkg/fd_funcs.h>
+
+using namespace stdex::literals;
+
+void commit(char const* rev, char const* fn)
+{
+	auto repo = vvpkg::repository(".", "r+");
+	vvpkg::managed_bundle<vvpkg::rabin_boundary> bs(10 * 1024 * 1024);
+	int fd = [&] {
+		if (fn == "-"_sv)
+			return vvpkg::xstdin_fileno();
+		else
+			return vvpkg::xopen_for_read(fn);
+	}();
+	defer(vvpkg::xclose(fd));
+
+	repo.commit(rev, bs, vvpkg::from_descriptor(fd));
+}
+
+void checkout(char const* rev, char const* fn)
+{
+	auto repo = vvpkg::repository(".", "r");
+	int fd = [&] {
+		if (fn == "-"_sv)
+			return vvpkg::xstdout_fileno();
+		else
+			return vvpkg::xopen_for_write(fn);
+	}();
+	defer(vvpkg::xclose(fd));
+
+	repo.checkout(rev, vvpkg::to_descriptor(fd));
+}
