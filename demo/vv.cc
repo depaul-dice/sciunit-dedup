2bf019867dc6d315c25b558b39370c0a8672ad4f
Raza Ahmad <sra0nasir@gmail.com>
2024-03-25 19:59:33 +0000

    Added code to print output size of the package. This was previously done through a patch.

--- demo/vv.cc
+++ demo/vv.cc
@@ -33,31 +33,31 @@ int main(int argc, char* argv[])
 	}
 }
 
-//void commit(char const* rev, char const* fn)
-//{
-//	auto repo = vvpkg::repository(".", "r+");
-//	vvpkg::managed_bundle<vvpkg::rabin_boundary> bs(10 * 1024 * 1024);
-//	int fd = [&] {
-//		if (fn == "-"_sv)
-//			return vvpkg::xstdin_fileno();
-//		else
-//			return vvpkg::xopen_for_read(fn);
-//	}();
-//	defer(vvpkg::xclose(fd));
-//
-//	repo.commit(rev, bs, vvpkg::from_descriptor(fd));
-//}
-//
-//void checkout(char const* rev, char const* fn)
-//{
-//	auto repo = vvpkg::repository(".", "r");
-//	int fd = [&] {
-//		if (fn == "-"_sv)
-//			return vvpkg::xstdout_fileno();
-//		else
-//			return vvpkg::xopen_for_write(fn);
-//	}();
-//	defer(vvpkg::xclose(fd));
-//
-//	repo.checkout(rev, vvpkg::to_descriptor(fd));
-//}
+void commit(char const* rev, char const* fn)
+{
+	auto repo = vvpkg::repository(".", "r+");
+	vvpkg::managed_bundle<vvpkg::rabin_boundary> bs(10 * 1024 * 1024);
+	int fd = [&] {
+		if (fn == "-"_sv)
+			return vvpkg::xstdin_fileno();
+		else
+			return vvpkg::xopen_for_read(fn);
+	}();
+	defer(vvpkg::xclose(fd));
+
+	std::cerr << repo.commit(rev, bs, vvpkg::from_descriptor(fd)) << std::endl;
+}
+
+void checkout(char const* rev, char const* fn)
+{
+	auto repo = vvpkg::repository(".", "r");
+	int fd = [&] {
+		if (fn == "-"_sv)
+			return vvpkg::xstdout_fileno();
+		else
+			return vvpkg::xopen_for_write(fn);
+	}();
+	defer(vvpkg::xclose(fd));
+
+	repo.checkout(rev, vvpkg::to_descriptor(fd));
+}
